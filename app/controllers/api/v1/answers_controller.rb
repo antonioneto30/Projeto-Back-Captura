1e3c01d0be154f45182280f89f0eb5be0ee41dda
Antonio <antonioneto3000@gmail.com>
2022-02-16 20:34:22 -0300

    V3

--- app/controllers/api/v1/answers_controller.rb
+++ app/controllers/api/v1/answers_controller.rb
@@ -15,8 +15,38 @@ module api
 
           def create
             @answer = Answer.new(answer_params)
+            if @answer.save
+                render json: @answer, status: :created, location: @answer
+              else
+                render json: @answer.errors, status: :unprocessable_entity
+
+          def update
+                    if @answer.update(answer_params)
+                      render json: @answer
+                    else
+                      render json: @answer.errors, status: :unprocessable_entity
+                    end
+                  end
+
+          def destroy
+                    @answer.destroy
+                  end 
+              end
+
+          private
+            #Use callbacks to share common setup or constraints between actions.
+            def set_answer
+                @answer = Answer.find(params[:id])
+              end    
+            end
+            
+            #Only allow a trusted parameter "white list" through.
+            def answer_params
+              params.permit(:content, :formulary_id, :question_id, :visit_id, :answered_at)
+            end
+          end
+           
 
         
-        end
     end
 end
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,2 +1,24 @@
 class answer < ApplicationRecord
+
+    belongs_to :formulary
+    belongs_to :question
+    belongs_to :visit
+    validates :content, presence: true
+    validates :question_id, presence: true
+    validates :formulary_id, presence: true
+    validate :question_is_valid?
+    validate :formulary_is_valid?
+
+    def question_is_valid?
+      if !Question.exists?(question_id)
+        errors.add(:question_id, "Essa pergunta não foi encontrada")
+      end
+    end
+
+    def formulary_is_valid?
+      if !Formulary.exists?(formulary_id)
+        errors.add(:formulary_id, "Esse formulário não foi encontrado")
+      end
+    end
+
 end
